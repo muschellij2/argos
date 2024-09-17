41bb51c701e13e0b56af38f6416d9e49d8800c8c
muschellij2 <muschellij2@gmail.com>
2024-09-17 00:14:49 -0400

    make it both

--- R/install_language_package.R
+++ R/install_language_package.R
@@ -26,7 +26,7 @@ install_language_package = function(
 
 
   argos = reticulate::import("argostranslate", convert = TRUE)
-  # apackage = reticulate::import("argostranslate.package", convert = TRUE)
+  apackage_temp = reticulate::import("argostranslate.package", convert = TRUE)
   apackage = argos$package
 
   if (!curl::has_internet()) {
